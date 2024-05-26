548c5466fa76f70b063a0d70e961393e3a6804df
Arabi <reza7494@ymail.com>
2024-05-26 13:22:20 +0330

    dddddddddddddddddd

--- resources/views/home/market/index.blade.php
+++ resources/views/home/market/index.blade.php
@@ -445,7 +445,7 @@
     <input type="hidden" id="previous_status-{{ $market->id }}" value="{{ $market->status }}">
 
     <div id="benchmark_info">
-        @include('home.market.benchmark_info',compact('now'))
+        @include('home.market.benchmark_info')
     </div>
     @include('home.market.winner_modal')
 
