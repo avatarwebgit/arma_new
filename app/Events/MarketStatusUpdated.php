acb25c22ebd734061cb5e9c133aa02fdc0418321
Arabi <reza7494@ymail.com>
2024-05-26 13:16:01 +0330

    ddddddddddddddddddddddddddd

--- app/Events/MarketStatusUpdated.php
+++ app/Events/MarketStatusUpdated.php
@@ -19,13 +19,14 @@ class MarketStatusUpdated implements  ShouldBroadcast
      *
      * @return void
      */
-    public $market_id,$difference,$timer,$status;
-    public function __construct($market_id,$difference,$timer,$status)
+    public $market_id,$difference,$timer,$status,$step;
+    public function __construct($market_id,$difference,$timer,$status,$step)
     {
         $this->market_id = $market_id;
         $this->difference = $difference;
         $this->timer = $timer;
         $this->status = $status;
+        $this->step = $step;
     }
 
     /**
