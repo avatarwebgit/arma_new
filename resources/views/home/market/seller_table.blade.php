b5ae54712b80e8321f9bcaa96f1d6f8490bd54fb
Arabi <reza7494@ymail.com>
2024-05-29 10:33:37 +0330

    sssssssssssssssssssssssss

--- resources/views/home/market/seller_table.blade.php
+++ resources/views/home/market/seller_table.blade.php
@@ -1,5 +1,9 @@
 <tr>
     <td class="text-center">{{ $market->SalesForm->max_quantity }}</td>
-    <td class="text-center">{{ $market->SalesForm->price }}</td>
+    @if($market->SalesForm->price_type=='Fix')
+        <td class="text-center">{{ $market->SalesForm->price }}</td>
+    @else
+        <td class="text-center">{{ $market->SalesForm->alpha  }}</td>
+    @endif
 {{--    <td class="text-center">{{ $market->offer_price }}</td>--}}
 </tr>
