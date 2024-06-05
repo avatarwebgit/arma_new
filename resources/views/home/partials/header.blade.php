49802bee00932519c9596e4c467db5ddd391aad2
Arabi <reza7494@ymail.com>
2024-06-05 10:12:35 +0330

    sssssssssssssssss

--- resources/views/home/partials/header.blade.php
+++ resources/views/home/partials/header.blade.php
@@ -11,41 +11,6 @@
         </button>
 
         <div class="collapse navbar-collapse" id="headerMenu">
-            <ul class="navbar-nav d-flex align-items-center">
-                @php
-                    $menus=\App\Models\Menus::where('parent',0)->get();
-                @endphp
-                @foreach($menus as $menu)
-                    @if($menu->id==2)
-                        <li class="nav-item d-flex align-items-center">
-                            <a class="nav-link" href="{{ route('home.menus',['menus'=>$menu->id]) }}"
-                               aria-haspopup="true"
-                               aria-expanded="false">
-                                {{ $menu->title }}
-                            </a>
-                        </li>
-                    @else
-                        <li class="nav-item dropdown d-flex align-items-center">
-                            <a class="nav-link dropdown-toggle" href="{{ route('home.menus',['menus'=>$menu->id]) }}"
-                               data-toggle="dropdown"
-                               aria-haspopup="true"
-                               aria-expanded="false">
-                                {{ $menu->title }}
-                            </a>
-                            @if(count($menu->children)>0)
-                                <div class="dropdown-menu">
-                                    @foreach($menu->children as $child)
-                                        <a class='dropdown-item' href='{{ route('home.menus',['menus'=>$child->id]) }}'>
-                                            {{ $child->title }}
-                                        </a>
-                                    @endforeach
-                                </div>
-                            @endif
-                        </li>
-                    @endif
-
-                @endforeach
-            </ul>
             <ul class="navbar-nav ml-auto d-flex align-items-center search_and_btns">
                 <li style="margin-left: auto" class="nav-item header-custom-icon position-relative mr-5">
                     <input class="form-control form-control-sm" placeholder="search...">
@@ -145,6 +110,41 @@
                         </li>
                     </ul>
                 @endauth
+            </ul>
+            <ul class="navbar-nav d-flex">
+                @php
+                    $menus=\App\Models\Menus::where('parent',0)->get();
+                @endphp
+                @foreach($menus as $menu)
+                    @if($menu->id==2)
+                        <li class="nav-item d-flex align-items-center">
+                            <a class="nav-link" href="{{ route('home.menus',['menus'=>$menu->id]) }}"
+                               aria-haspopup="true"
+                               aria-expanded="false">
+                                {{ $menu->title }}
+                            </a>
+                        </li>
+                    @else
+                        <li class="nav-item dropdown d-flex align-items-center">
+                            <a class="nav-link dropdown-toggle" href="{{ route('home.menus',['menus'=>$menu->id]) }}"
+                               data-toggle="dropdown"
+                               aria-haspopup="true"
+                               aria-expanded="false">
+                                {{ $menu->title }}
+                            </a>
+                            @if(count($menu->children)>0)
+                                <div class="dropdown-menu">
+                                    @foreach($menu->children as $child)
+                                        <a class='dropdown-item' href='{{ route('home.menus',['menus'=>$child->id]) }}'>
+                                            {{ $child->title }}
+                                        </a>
+                                    @endforeach
+                                </div>
+                            @endif
+                        </li>
+                    @endif
+
+                @endforeach
             </ul>
         </div>
     </nav>
