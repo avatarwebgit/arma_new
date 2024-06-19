5dbfe9e81ebe2c8c3d6a159be308b08600c808e0
Arabi <reza7494@ymail.com>
2024-06-19 14:40:53 +0330

    ytyty

--- resources/views/home/index/index.blade.php
+++ resources/views/home/index/index.blade.php
@@ -238,18 +238,6 @@
         function slidemore(market_id, event) {
             event.stopPropagation();
             $('#more_table_' + market_id).slideToggle();
-            let svg = $('#slide_more_angle_' + market_id).find('svg');
-            let hasClass = svg.hasClass('fa-angle-down');
-            if (hasClass) {
-                svg.removeClass('fa-angle-down');
-                svg.addClass('fa-angle-up');
-            } else {
-                svg.removeClass('fa-angle-up');
-                svg.addClass('fa-angle-down');
-            }
-        }
-
-        function ToggleShowMoreInformation(market_id) {
             $.ajax({
                 url: "{{ route('home.market_more_info') }}",
                 data: {
@@ -267,7 +255,17 @@
                     }
                 }
             });
+            let svg = $('#slide_more_angle_' + market_id).find('svg');
+            let hasClass = svg.hasClass('fa-angle-down');
+            if (hasClass) {
+                svg.removeClass('fa-angle-down');
+                svg.addClass('fa-angle-up');
+            } else {
+                svg.removeClass('fa-angle-up');
+                svg.addClass('fa-angle-down');
+            }
         }
+
     </script>
 @endsection
 
---
+++ resources/views/home/partials/market_more.blade.php
@@ -0,0 +1,106 @@
+<td colspan="11">
+    <table class="table_in_table" style="width: 100%">
+        <tr>
+            <td class="text-center">
+                <span class="text-bold">Contract Type</span>
+                <span>
+                                    @if($is_login)
+                        {{ $market->SalesForm->contract_type }}
+                    @else
+                        Log in/Register
+                    @endif
+                                </span>
+            </td>
+            <td class="text-center">
+                <span class="text-bold">Min Order</span>
+                <span>
+                                    @auth
+                        {{ $market->SalesForm->min_order }}
+                    @else
+                        Log in/Register
+                    @endauth
+                                </span>
+            </td>
+
+            <td class="text-center">
+                <span class="text-bold">Supplier</span>
+                <span>
+                                    @auth
+                        {{ $market->SalesForm->company_type }}
+                    @else
+                        Log in/Register
+                    @endauth
+                                </span>
+            </td>
+
+
+        </tr>
+        <tr>
+            <td class="text-center">
+                <span class="text-bold">Specification</span>
+                <span>
+                                                                @auth
+                        Available
+                    @else
+                        Log in/Register
+                    @endauth
+                                                            </span>
+            </td>
+            <td class="text-center">
+                <span class="text-bold">Price Type</span>
+                <span>
+                                    @auth
+                        {{ $market->SalesForm->price_type }}
+                    @else
+                        Log in/Register
+                    @endauth
+                                </span>
+            </td>
+            <td class="text-center">
+                <span class="text-bold">Payment</span>
+                <span>
+                                    @auth
+                        {{ $market->SalesForm->payment }}
+                    @else
+                        Log in/Register
+                    @endauth
+                                </span>
+            </td>
+
+        </tr>
+        <tr>
+            <td class="text-center">
+                <span class="text-bold">Offer Price</span>
+                <span>
+                                                                @auth
+                        Available
+                    @else
+                        Log in/Register
+                    @endauth
+                                                            </span>
+            </td>
+
+            <td class="text-center">
+                <span class="text-bold">Unit</span>
+                <span>
+                                    @auth
+                        {{ $market->SalesForm->unit }}
+                    @else
+                        Log in/Register
+                    @endauth
+                                </span>
+            </td>
+
+            <td class="text-center">
+                <span class="text-bold">Currency</span>
+                <span>
+                                        @auth
+                        {{ $market->SalesForm->currency }}
+                    @else
+                        Log in/Register
+                    @endauth
+                                </span>
+            </td>
+        </tr>
+    </table>
+</td>
