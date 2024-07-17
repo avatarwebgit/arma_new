8c0f08de2f28c69beb1b8c448c7adb0bdb5b6a5e
Arabi <reza7494@ymail.com>
2024-07-17 13:34:54 +0330

    okokoko

--- resources/views/home/timer/index.blade.php
+++ resources/views/home/timer/index.blade.php
@@ -1,7 +1,7 @@
 <div class="timer_index clock" @if($timer_is_red==1)  style="color: #c20000 !important;" @endif>
     <div class="column">
         <div class="timer">{{ $hours }}</div>
-        <div class="text hour">HR</div>
+        <div class="text hour" style="margin-right: 50px !important;">HR</div>
     </div>
     <div style="font-family:none !important" class="timer">:</div>
     <div class="column">
