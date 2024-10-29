81830943b342746c22ba8f7ba214705961e6b56a
Arabi <reza7494@ymail.com>
2024-10-29 13:14:48 +0330

    pppp

--- resources/views/home/market/term_condition.blade.php
+++ resources/views/home/market/term_condition.blade.php
@@ -1,12 +1,13 @@
+<div class="bid_term_condition p-3 text-justify mt-3">
+    {!! $market->SalesForm->term_conditions !!}
+</div>
 <div class="form-check mt-5">
-    <input class="form-check-input" type="checkbox" value="1" id="CheckTermCondition_{{ $market->id }}" name="term_condition">
+    <input class="form-check-input" type="checkbox" value="1" id="CheckTermCondition_{{ $market->id }}"
+           name="term_condition">
     <label class="form-check-label" for="flexCheckDefault">
         <strong>
             I Read and accept terms and conditions
         </strong>
         <span id="accept_term_alert" style="font-size: 9pt;color: red" class="ml-3">Accept Term and Conditions</span>
     </label>
-</div>
-<div class="bid_term_condition p-3 text-justify mt-3">
-    {!! $market->SalesForm->term_conditions !!}
 </div>
