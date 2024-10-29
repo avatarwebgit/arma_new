b9c2fc65521ee53d1fe79ff59eb821a81e924b77
Arabi <reza7494@ymail.com>
2024-10-29 11:39:15 +0330

    wwww

--- resources/views/home/market/market_info.blade.php
+++ resources/views/home/market/market_info.blade.php
@@ -73,7 +73,7 @@
     <div class="d-flex justify-content-between">
         <span class="text-bold text-gray-100">Delivery Period</span>
         <span class="text-bold text-light-blue ">
-            {{ 'from: '.$market->SalesForm->loading_from.' - to: '.$market->SalesForm->loading_to }}
+            {{ 'from: '.$market->SalesForm->loading_from.'    to: '.$market->SalesForm->loading_to }}
         </span>
     </div>
 
