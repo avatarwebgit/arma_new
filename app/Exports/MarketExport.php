a4c26237b00a9d9056c2804c4773d23f5e9e2d41
Arabi <reza7494@ymail.com>
2024-11-12 16:34:23 +0330

    lll

--- app/Exports/MarketExport.php
+++ app/Exports/MarketExport.php
@@ -19,7 +19,50 @@ class MarketExport implements FromCollection, WithHeadings, WithColumnWidths
 
     public function collection()
     {
-        return $this->markets;
+        $items = [];
+        $markets = $this->markets;
+        foreach ($markets as $market) {
+            //
+            $bid = $market->Bids()->orderBy('price', 'desc')->first();
+
+            $has_winner = $market->Bids()->where('is_win', 1)->exists();
+            if ($has_winner) {
+                $status_color = 'green';
+                $status_text = 'Done';
+            } else {
+                $status_color = 'red';
+                $status_text = 'Failed';
+            }
+            if ($market->SalesForm->unit == 'other' or $market->SalesForm->unit == 'Other') {
+                $unit = $market->SalesForm->unit_other;
+            } else {
+                $unit = $market->SalesForm->unit;
+            }
+            if ($market->SalesForm->currency == 'other' or $market->SalesForm->currency == 'Other') {
+                $currency = $market->SalesForm->currency_other . '/' . $unit;
+            } else {
+                $currency = $market->SalesForm->currency . '/' . $unit;
+            }
+
+            if ($bid) {
+
+                $highest = $bid->price . ' ' . $currency;
+                $quantity = $bid->quantity . ' ' . $unit;
+            } else {
+                $highest = 0;
+                $quantity = 0;
+            }
+            //
+            $minQuantity = str_replace(',', '', $market->SalesForm->min_order);
+
+            $items['Data'] = $market->date;
+            $items['Commodity'] = $market->SalesForm->commodity;
+            $items['Quantity'] = $market->SalesForm->max_quantity . ' ' . $unit;
+            $items['Min Order'] = number_format($minQuantity) . ' ' . $unit;
+            $items['Packing'] = $market->SalesForm->packing;
+            $items['Delivery'] = $market->SalesForm->packing
+        }
+        return $items;
     }
 
     public function columnWidths(): array
@@ -37,36 +80,24 @@ class MarketExport implements FromCollection, WithHeadings, WithColumnWidths
             'J' => 20,
             'K' => 20,
             'L' => 20,
-            'M' => 20,
-            'N' => 20,
-            'O' => 20,
-            'P' => 20,
-            'Q' => 20,
-            'R' => 20,
-            'S' => 20,
         ];
     }
 
     public function headings(): array
     {
         return [
-            "row_id",
-            "order_id",
-            "شماره سفارش",
-            "تاریخ سفارش",
-            "نام و نام خانوادگی مشتری",
-            "موبایل مشتری",
-            "کدملی مشتری",
-            "تلفن مشتری",
-            "سفارشات",
-            "قیمت واحد کالا(تومان)",
-            "اقلام همراه",
-            "قیمت اقلام همراه",
-            "تعداد",
-            "جمع هر مدل کالا(تومان)",
-            "مبلغ سفارش(تومان)",
-            "هزینه حمل",
-            "آدرس مشتری",
+            "Data",
+            "Commodity",
+            "Quantity",
+            "Min Order",
+            "Packing",
+            "Delivery",
+            "Region",
+            "Price Type",
+            "Offer Price",
+            "Highest Bid",
+            "Quantity",
+            "Status",
         ];
     }
 }
