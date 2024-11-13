8d961aa9dd48cad81a7337e0deb7f2a9455871cd
Arabi <reza7494@ymail.com>
2024-11-13 15:26:24 +0330

    wwww

--- resources/views/home/daily_report/row.blade.php
+++ resources/views/home/daily_report/row.blade.php
@@ -1,109 +1,39 @@
-@foreach($markets as $market)
-    @php
-        $bid=$market->Bids()->orderBy('price','desc')->first();
+@php
+    $groupedMarkets = $markets->groupBy('date');
+@endphp
 
-        $has_winner=$market->Bids()->where('is_win',1)->exists();
-        if ($has_winner){
-            $status_color='green';
-            $status_text='Done';
-        }else{
-             $status_color='red';
-            $status_text='Failed';
-        }
-        if ($market->SalesForm->unit=='other' or $market->SalesForm->unit=='Other'){
-            $unit=$market->SalesForm->unit_other;
-        }else{
-            $unit=$market->SalesForm->unit;
-        }
-        if ($market->SalesForm->currency=='other' or $market->SalesForm->currency=='Other'){
-            $currency=$market->SalesForm->currency_other.'/'.$unit;
-        }else{
-            $currency=$market->SalesForm->currency.'/'.$unit;
-        }
-
-        if ($bid){
-
-            $highest=number_format($bid->price).' '.$currency;
-            $quantity=number_format($bid->quantity).' '.$unit;
-        }else{
-            $highest=0;
-            $quantity=0;
-        }
-
-    @endphp
-    <tr class="{{ $status_color }}">
-        <td>
-              <span>
-            {{ $market->date }}
-            </span>
-        </td>
-        <td>
-            <span>
-            {{ $market->SalesForm->commodity }}
-            </span>
-        </td>
-
-        <td>
-            <span>
-
-             {{ $market->SalesForm->max_quantity.' '.$unit }}
-            </span>
-        </td>
-        <td>
-             <span>
-                  @php
-                      $minQuantity=str_replace(',','',$market->SalesForm->min_order);
-                  @endphp
-                 {{ number_format($minQuantity).' '.$unit }}
-            </span>
-        </td>
-        <td>
-            <span>
-             {{ $market->SalesForm->packing }}
-            </span>
-        </td>
-        <td>
-             <span>
-            {{ $market->SalesForm->incoterms }}
-            </span>
-        </td>
-        <td>
-                        <span>
-
-            {{ $market->SalesForm->origin_country }}
-            </span>
-        </td>
-        <td>
-                        <span>
-
-            {{ $market->SalesForm->price_type }}
-            </span>
-        </td>
-        @if($market->SalesForm->price_type=='Fix')
-            <td>{{ number_format($market->SalesForm->price).' '.$currency }}</td>
-        @else
-            <td>{{ number_format($market->SalesForm->alpha) .' '.$currency }}</td>
-        @endif
-        <td>
-                        <span>
-
-
-
-                            {{ $highest==0 ? 'n/a' : $highest }}
-
-            </span>
-        </td>
-        <td>
-                        <span>
-
-           {{ $quantity==0? 'n/a' : $quantity }}
-            </span>
-        </td>
-        <td>
-                        <span>
-
-           {{ $status_text }}
-            </span>
-        </td>
+@foreach($groupedMarkets as $date => $marketsByDate)
+    <tr>
+        <td colspan="12"><strong>{{ $date }}</strong></td>
     </tr>
+    @foreach($marketsByDate->sortBy('time') as $market)
+        @php
+            $bid = $market->Bids()->orderBy('price', 'desc')->first();
+            $has_winner = $market->Bids()->where('is_win', 1)->exists();
+            $status_color = $has_winner ? 'green' : 'red';
+            $status_text = $has_winner ? 'Done' : 'Failed';
+            $unit = $market->SalesForm->unit === 'other' ? $market->SalesForm->unit_other : $market->SalesForm->unit;
+            $currency = $market->SalesForm->currency === 'other' ? $market->SalesForm->currency_other . '/' . $unit : $market->SalesForm->currency . '/' . $unit;
+            $highest = $bid ? number_format($bid->price) . ' ' . $currency : 'n/a';
+            $quantity = $bid ? number_format($bid->quantity) . ' ' . $unit : 'n/a';
+            $minQuantity = str_replace(',', '', $market->SalesForm->min_order);
+        @endphp
+        <tr class="{{ $status_color }}">
+            <td>{{ $market->date }}</td>
+            <td>{{ $market->SalesForm->commodity }}</td>
+            <td>{{ $market->SalesForm->max_quantity . ' ' . $unit }}</td>
+            <td>{{ number_format($minQuantity) . ' ' . $unit }}</td>
+            <td>{{ $market->SalesForm->packing }}</td>
+            <td>{{ $market->SalesForm->incoterms }}</td>
+            <td>{{ $market->SalesForm->origin_country }}</td>
+            <td>{{ $market->SalesForm->price_type }}</td>
+            <td>
+                {{ $market->SalesForm->price_type === 'Fix' ? number_format($market->SalesForm->price) . ' ' . $currency : number_format($market->SalesForm->alpha) . ' ' . $currency }}
+            </td>
+            <td>{{ $highest }}</td>
+            <td>{{ $quantity }}</td>
+            <td>{{ $status_text }}</td>
+        </tr>
+    @endforeach
 @endforeach
+
