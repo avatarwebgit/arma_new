33727069b8fcd6f6ba895acb4317add7148866a4
Arabi <reza7494@ymail.com>
2024-11-19 15:06:29 +0330

    ssss

--- app/Http/Controllers/Home/MarketHomeController.php
+++ app/Http/Controllers/Home/MarketHomeController.php
@@ -603,6 +603,7 @@ class MarketHomeController extends Controller
             $market = Market::where('id', $market_id)->first();
             $bidhistories_groups = $market->Bids()->orderby('price', 'desc')->get()->groupby('price');
             $ids = $this->BidWinner($market);
+            dd($ids);
             $bids = [];
             $max_quantity = str_replace(',', '', $market->SalesForm->max_quantity);
             $remain_quantity = $max_quantity;
