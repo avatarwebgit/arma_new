aa5d4b443b25dea3340f776754a59142e0682b1d
Arabi <reza7494@ymail.com>
2024-11-19 15:38:19 +0330

    aaaa

--- resources/views/home/daily_report/row.blade.php
+++ resources/views/home/daily_report/row.blade.php
@@ -19,21 +19,31 @@
                     $has_winner=true;
                 }
             }
+
+            if ($bid){
+                $quantity_show=$bid->quantity;
+            if ($has_winner){
+                $quantity_show=$market->Bids()->where('is_win', 1)->sum('quantity');
+            }
+            }else{
+                $quantity_show=null;
+            }
+
              $status_color = $has_winner ? 'green' : 'red';
             $status_text = $has_winner ? 'Done' : 'Failed';
             $unit = $market->SalesForm->unit === 'other' ? $market->SalesForm->unit_other : $market->SalesForm->unit;
             $currency = $market->SalesForm->currency === 'other' ? $market->SalesForm->currency_other . '/' . $unit : $market->SalesForm->currency . '/' . $unit;
             $highest = $bid ? number_format($bid->price) . ' ' . $currency : 'n/a';
-            $quantity = $bid ? number_format($bid->quantity) . ' ' . $unit : 'n/a';
+            $quantity = $bid ? number_format($quantity_show) . ' ' . $unit : 'n/a';
             $minQuantity = str_replace(',', '', $market->SalesForm->min_order);
             $marketDateTime = $market->date;
         @endphp
 
         @if($market->date !== $today || $market->time < $time)
             <tr
-                    style="cursor: pointer"
-                    onclick="ShowBidPage({{ $market->id }})"
-                    class="{{ $status_color }}">
+                style="cursor: pointer"
+                onclick="ShowBidPage({{ $market->id }})"
+                class="{{ $status_color }}">
                 <td>{{ $marketDateTime }}</td>
                 <td>{{ \Carbon\Carbon::parse($market->time)->format('H:i') }}</td>
 
