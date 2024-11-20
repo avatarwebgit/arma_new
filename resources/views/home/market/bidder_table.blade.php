893642d007387d26a57a130bd4b6d6dddc6cec29
Arabi <reza7494@ymail.com>
2024-11-20 10:53:19 +0330

    delete button style

--- resources/views/home/market/bidder_table.blade.php
+++ resources/views/home/market/bidder_table.blade.php
@@ -15,16 +15,24 @@
                 {{ number_format($bid->quantity) }}
            </span>
             </td>
-            <td class="text-center">
+            <td class="text-center" style="position: relative">
                 {{ number_format($bid->price) }}
                 @if($key!=0 )
                     @if($bid->user_id==auth()->id() and $bid->Market->status==3 )
 
-                        <span id="remove_btn_{{ $market->id }}" onclick="removeBid({{ $market->id }},{{ $bid->id }})">
+                        <span id="remove_btn_{{ $market->id }}" onclick="removeBid({{ $market->id }},{{ $bid->id }})"
+                              style="
+                              background: red;
+  padding: 2px 9px;
+  border-radius: 5px;
+  font-size: 7pt;
+  position: absolute;
+  right: 2px;
+  bottom: 9px;">
 {{--                     <i  class="fa fa-times-circle text-danger"></i>--}}
-                            <span style="display: block;padding: 5px 10px;background-color: red;color: white">
+
                                 Delete
-                            </span>
+
                 </span>
                     @endif
                 @endif
