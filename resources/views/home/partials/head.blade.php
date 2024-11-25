8c28ba8e1d847de1ad5f77ca53b8f2917dc80fcd
avatarwebgit <121947853+avatarwebgit@users.noreply.github.com>
2024-11-25 10:48:39 +0330

    Update head.blade.php

--- resources/views/home/partials/head.blade.php
+++ resources/views/home/partials/head.blade.php
@@ -44,8 +44,13 @@
 
 
     @vite(['resources/sass/app.scss','resources/js/app.js'])
-
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
     <style>
+        body{
+  font-family: "Merriweather", serif !important;
+        }
         .ion-logo-x::before {
             content: "x" !important;
             font-size: 29px;
