3f94d0afdf7c522b3a2c8eff7f3dd6aa2a48c067
avatarwebgit <121947853+avatarwebgit@users.noreply.github.com>
2024-12-09 15:09:57 +0330

    Create MarketHistory.php

---
+++ app/Models/MarketHistory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MarketHistory extends Model
+{
+    use HasFactory;
+
+ 
+    protected $table = 'market_history';
+
+    protected $guarded = [];
+
+    
+   
+
+    
+}
