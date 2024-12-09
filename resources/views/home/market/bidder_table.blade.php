095149e2cb434c4e9715df740a13897d723f8537
avatarwebgit <121947853+avatarwebgit@users.noreply.github.com>
2024-12-09 11:35:58 +0330

    Update bidder_table.blade.php

--- resources/views/home/market/bidder_table.blade.php
+++ resources/views/home/market/bidder_table.blade.php
@@ -4,10 +4,26 @@
         <td class="text-center">{{ $bid->price }}</td>
         <td class="text-center">
             @if($bid->Market->status==3)
+
                 @if($key!=0 and $bid->user_id==auth()->id())
+                        @php
+            $is_delete = false;
+             $bid_exists = $market->Bids()->exists();
+                if ($bid_exists) {
+                    $highest_price = $market->Bids()->orderBy('price', 'desc')->first();
+                    $highest_price = $highest_price->price;
+                    if ($bid->price == $highest_price) {
+            $is_delete =true;
+                   }
+                }
+
+            @endphp
+            @if($is_delete)
                     <span class="remove_function" onclick="removeBid({{ $bid->id }})">
                          <i  class="fa fa-times-circle text-danger"></i>
                     </span>
+            @endif
+
                 @endif
             @endif
 
