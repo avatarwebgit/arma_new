af55e3836e68d06f61c941f95506ee28b6391002
avatarwebgit <121947853+avatarwebgit@users.noreply.github.com>
2024-12-09 11:32:47 +0330

    Update bidder_table.blade.php

--- resources/views/home/market/bidder_table.blade.php
+++ resources/views/home/market/bidder_table.blade.php
@@ -3,11 +3,14 @@
         <td class="text-center">{{ $bid->quantity }}</td>
         <td class="text-center">{{ $bid->price }}</td>
         <td class="text-center">
-            @if(($key!=0 and $bid->user_id==auth()->id()) and $bid->Market->status==3 )
-                <span class="remove_function" onclick="removeBid({{ $bid->id }})">
-                     <i  class="fa fa-times-circle text-danger"></i>
-                </span>
+            @if($bid->Market->status==3)
+                @if($key!=0 and $bid->user_id==auth()->id())
+                    <span class="remove_function" onclick="removeBid({{ $bid->id }})">
+                         <i  class="fa fa-times-circle text-danger"></i>
+                    </span>
+                @endif
             @endif
+
         </td>
     </tr>
 @endforeach
