b59343478670d797b2316df10e070313f0d8d0f7
Mohammad Mehdi <105783397+Mehdialibakhshi03@users.noreply.github.com>
2024-12-17 10:05:54 +0330

    Update bidder_table.blade.php

--- resources/views/home/market/bidder_table.blade.php
+++ resources/views/home/market/bidder_table.blade.php
@@ -68,17 +68,6 @@
                             @endif
 
                         @elseif($is_first == 1 and ($best_price->bid_id != $bid->id))
-
-                        @php
-                            $is_delete = false;
-                             $bids = $market->Bids;
-                $bid_exists_with_same_price = $bids->filter(function ($other_bid) use ($bid) {
-                    return $other_bid->price == $bid->price && $other_bid->id != $bid->id; // مطمئن شویم که خود bid فعلی را در نظر نمی‌گیریم
-                })->isNotEmpty();
-dd($bid_exists_with_same_price);
-             
-
-                        @endphp
                             @if(!$bid_history_delete)
 
                                 <span id="remove_btn_{{ $market->id }}"
