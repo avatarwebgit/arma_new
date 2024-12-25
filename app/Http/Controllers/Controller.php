a453fc113e31140ffd0bc422540468490fea395b
Mohammad Mehdi <105783397+Mehdialibakhshi03@users.noreply.github.com>
2024-12-25 12:28:13 +0330

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -405,7 +405,7 @@ class Controller extends BaseController
 
 // تاریخ‌های سه روز آینده
 
-  dd($nextThreeDays);
+
 
 // حالا بررسی می‌کنیم که برای هر روز آیا مارکت داریم یا نه
 foreach ($nextThreeDays as $index => $day) {
@@ -420,7 +420,10 @@ foreach ($nextThreeDays as $index => $day) {
         // از روز 4 ام به بعد به مدت 3 روز بررسی می‌کنیم
         for ($i = 4; $i <= 19; $i++) {
             $futureDay = $today->copy()->addDays($i); // روزهای بعد از روز 4 ام
-
+            if($i == 5 or $i == 6){
+                 dd($futureDay);
+            }
+           
             $futureFormatted = $futureDay->format('Y-m-d');
             
              $marketsForThisDay = Market::where('date', $futureFormatted)->get();
