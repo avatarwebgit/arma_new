81c0c0872d04afee8ad85bfb8141a0a6344f27a6
Mohammad Mehdi <105783397+Mehdialibakhshi03@users.noreply.github.com>
2025-01-08 09:59:02 +0330

    Update buyer_sidebar.blade.php

--- resources/views/admin/layouts/buyer_sidebar.blade.php
+++ resources/views/admin/layouts/buyer_sidebar.blade.php
@@ -27,3 +27,32 @@
         </li>
     </ul>
 </li>
+
+
+    <li class="dash-item dash-hasmenu {{ request()->is('admin-panel/management/orders*') ? 'active dash-trigger' : 'collapsed' }}">
+        <a href="#!" class="dash-link">
+            <span class="dash-micon"><i class="fas fa-box"></i></span>
+            <span class="dash-mtext">{{ __('Sales Order') }}</span>
+            <span class="dash-arrow"><i data-feather="chevron-right"></i></span>
+        </a>
+        <ul class="dash-submenu">
+            <li class="dash-item">
+                <a class="dash-link" href="{{ route('sale_form', ['page_type' => 'Create']) }}">
+                    <span class="dash-micon"><i class="fas fa-file-alt"></i></span>
+                    New
+                </a>
+            </li>
+            <li class="dash-item">
+                <a class="dash-link" href="{{ route('sale_form_list',['type'=>'Save']) }}">
+                    <span class="dash-micon"><i class="fas fa-file-alt"></i></span>
+                    Save ( {{ $SalesFormCounts['Save'] }} )
+                </a>
+            </li>
+            <li class="dash-item">
+                <a class="dash-link" href="{{ route('sale_form_list',['type'=>'Draft']) }}">
+                    <span class="dash-micon"><i class="fas fa-file-alt"></i></span>
+                    Draft ( {{ $SalesFormCounts['Draft'] }} )
+                </a>
+            </li>
+        </ul>
+    </li>
