329fbb390484c42183113735e1b778cf49d96ce7
Mohammad Mehdi <105783397+Mehdialibakhshi03@users.noreply.github.com>
2025-01-08 12:58:01 +0330

    Update SellerLinkedToMarket.php

--- app/Events/SellerLinkedToMarket.php
+++ app/Events/SellerLinkedToMarket.php
@@ -3,9 +3,12 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\ShouldBroadcast;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+
 
 class SellerLinkedToMarket implements ShouldBroadcast
 {
