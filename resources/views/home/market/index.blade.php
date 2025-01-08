5555673fdf49fcf0153be1a55ed72eb3e1c4123a
Mohammad Mehdi <105783397+Mehdialibakhshi03@users.noreply.github.com>
2025-01-08 12:59:19 +0330

    Update index.blade.php

--- resources/views/home/market/index.blade.php
+++ resources/views/home/market/index.blade.php
@@ -10,7 +10,7 @@
     <script type="module">
 window.Echo.channel('Seller-Linked-To-Market')
     .listen('SellerLinkedToMarket', (event) => {
-console.log(event);
+console.log('event');
         // وقتی رویداد ارسال شد، بررسی کنید و وضعیت نمایش را به‌روزرسانی کنید
         if (event.sellerId.includes('{{ auth()->user()->id }}') ) {
             // تغییر وضعیت نمایش برای سلر
