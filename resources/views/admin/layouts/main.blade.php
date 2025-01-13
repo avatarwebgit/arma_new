b0b79f13b215e8927c37e46fa0eb8b82736eb21c
Mohammad Mehdi <105783397+Mehdialibakhshi03@users.noreply.github.com>
2025-01-13 13:26:42 +0330

    Update main.blade.php

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -350,8 +350,8 @@
         $('#CreateMarketModal').modal('show');
     }
     function ModalResetPassword(id,user_name) {
-        console.log(user_name);
-        $('#user_name').val(user_name);
+     
+        $('#userName').val(user_name);
         $('#reset_password_modal').modal('show');
     }
 
